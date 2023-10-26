841c598ad5c77d12fcc093107ac44034082394f6
Leonardo <leonardojfs83@gmail.com>
2023-10-25 21:43:56 -0300

    Correcion Rutas

--- proyecto-integrador/src/Components/Header.jsx
+++ proyecto-integrador/src/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <header className='bg-orange-500 fixed top-0 left-0 right-0 top-0'>
             <div className='container mx-auto px-4 py-2 flex flex-col md:flex-row items-center justify-between'>
                 <div className='flex items-center mb-2 md:mb-0'>
-                    <Link to="/home"><img className='h-16 md:h-20' src={logo} alt="logoEmpresa" /></Link>
+                    <Link to="./"><img className='h-16 md:h-20' src={logo} alt="logoEmpresa" /></Link>
                     <div className="ml-2 md:ml-4">
                         <h1 className="text-2xl md:text-3xl font-bold text-blue">Musify</h1>
                         <p className="hidden md:block text-sm text-blue">Donde la música y la pasión se encuentran</p>
--- proyecto-integrador/src/main.jsx
+++ proyecto-integrador/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import App from './App.jsx'
+import Home from './Routes/Home.jsx'
+import LoginSignUp from './Routes/LoginSignUp.jsx'
+import Products from './Routes/Products.jsx'
+import Nopage from './Routes/Nopage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/login" element={<LoginSignUp />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<Nopage />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  </>,
 )
