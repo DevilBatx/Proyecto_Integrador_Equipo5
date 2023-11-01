1ad0e27fc4a5093d2d577cf53cf67f0bccde6a60
nacholima <lolnacho.lima@gmail.com>
2023-10-31 22:45:42 -0300

    frontend hash routing updated for s3 health

--- proyecto-integrador-musify-front/src/main.jsx
+++ proyecto-integrador-musify-front/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.jsx'
 import Home from './Routes/Home.jsx'
 import LoginSignUp from './Routes/LoginSignUp.jsx'
@@ -12,21 +12,23 @@ import AgregarProducto from './pages/AgregarProducto.jsx'
 import ListAllProductsPage from './pages/ListAllProductsPage.jsx'
 import './index.css'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>}>
-        <Route index element={<Home/>}/>
-        <Route path="/login" element={<LoginSignUp />} />
-        <Route path="/details/:id" element={<Detail />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/administracion"element = {<AdminPage />} />
-        <Route path="/allproducts"element = {<ListAllProductsPage />} />
-        <Route path="/agregarproducto"element = {<AgregarProducto />} />
-        <Route path="*" element={<Nopage />} />
-      </Route>
-    </Routes>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}>
+          <Route index element={<Home/>}/>
+          <Route path="/login" element={<LoginSignUp />} />
+          <Route path="/details/:id" element={<Detail />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/administracion" element={<AdminPage />} />
+          <Route path="/allproducts" element={<ListAllProductsPage />} />
+          <Route path="/agregarproducto" element={<AgregarProducto />} />
+          <Route path="*" element={<Nopage />} />
+        </Route>
+      </Routes>
+    </Router>
   </>,
-)
+);
