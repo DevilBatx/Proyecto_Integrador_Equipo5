cd21b8fb583bc6f846961885ae6c296188c3518d
DevilBatx <llc.leonardolopez@gmail.com>
2023-11-23 01:35:38 -0300

    Mirror

--- proyecto-integrador-musify-front/src/App.jsx
+++ proyecto-integrador-musify-front/src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer';
 import { ContextProvider } from './Components/Utils/GlobalContext';
 
-//Mirrorr
+//Mirrorrr
 
 
 function App() {
