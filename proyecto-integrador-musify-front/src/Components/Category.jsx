690af3b7e12dffe0854246e93568aa3cbf7d8b15
DevilBatx <llc.leonardolopez@gmail.com>
2023-12-04 21:12:53 -0300

    Merge de todito

--- proyecto-integrador-musify-front/src/Components/Category.jsx
+++ proyecto-integrador-musify-front/src/Components/Category.jsx
@@ -34,34 +34,8 @@ const Category = () => {
                                 Explorar Categoria
                             </Link>
                         </div>
-                        <h5 className="text-2xl  text-center md:text-3xl font-medium mt-3">Accesorios</h5>
-                        <a href="#" className=' text-white mt-5 bg-gradient-to-b from-[#D97236] via-[#D97236] to-[#F2A649] hover:bg-blue-800 focus:ring-3 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:bg-orange-500 dark:hover:bg-orange-100 dark:hover:text-orange-500 dark:focus:ring-blue-800'>Explorar Categoria</a>
                     </div>
-                </div>
-                {/*card3*/}
-                <div className='card'>
-                    <div className='p-5 flex flex-col'>
-                        <div className='rounded-xl overflow-hidden'>
-                            <Link to="/guitarras">
-                                <img className=" w-29" src={Piano} alt="product image" />
-                            </Link>
-                        </div>
-                        <h5 className="text-2xl  text-center md:text-3xl font-medium mt-3">Teclados</h5>
-                        <a href="#" className='text-white mt-5 bg-gradient-to-b from-[#D97236] via-[#D97236] to-[#F2A649] hover:bg-blue-800 focus:ring-3 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:bg-orange-500 dark:hover:bg-orange-100 dark:hover:text-orange-500 dark:focus:ring-blue-800 '>Explorar Categoria</a>
-                    </div>
-                </div>
-                {/*card4*/}
-                <div className='card'>
-                    <div className='p-5 flex flex-col'>
-                        <div className='rounded-xl overflow-hidden'>
-                            <Link to="/guitarras">
-                                <img className=" w-29" src={bateria2} alt="product image" />
-                            </Link>
-                        </div>
-                        <h5 className="text-2xl  text-center md:text-3xl font-medium mt-3">Percusion</h5>
-                        <a href="#" className=' text-white mt-5 bg-gradient-to-b from-[#D97236] via-[#D97236] to-[#F2A649] hover:bg-blue-800 focus:ring-3 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:bg-orange-500 dark:hover:bg-orange-100 dark:hover:text-orange-500 dark:focus:ring-blue-800'>Explorar Categoria</a>
-                    </div>
-                </div>
+                ))}
             </div>
         </div >
     );
